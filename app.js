41d57bf192e7fb6a0cc9806391451c6e3b557289
21120612 <nmt6465@gmail.com>
2023-12-27 23:57:51 +0700

    set rate limit ip access route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
 dotenv.config({ path: './config.env' });
 const tourRouter = require('./routers/tourRouter.js');
 const ErrorHandlerController = require('./controllers/ErrorController.js');
@@ -9,12 +10,21 @@ const appError = require('./utils/appError.js');
 const app = express();
 process.noDeprecation = true;
 
+// 1) global middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'too many request from this ip, please try again in an hour',
+});
+
+app.use('/api', limiter);
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
