81efc8df6f3df85b2cb1c12e272179f3959df828
21120612 <nmt6465@gmail.com>
2024-01-14 07:44:45 +0700

    create nested route reviews on tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,8 +3,9 @@ const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 
 module.exports.getAllReview = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
-
+  let filter = {};
+  if (req.params.tourID) filter = { tour: req.params.tourID };
+  const reviews = await Review.find(filter);
   res.status(200).json({
     status: 'success',
     length: reviews.length,
@@ -15,6 +16,7 @@ module.exports.getAllReview = catchAsync(async (req, res, next) => {
 });
 
 module.exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourID;
   if (!req.body.user) req.body.user = req.user.id;
   const review = await Review.create(req.body);
